efb4f1607cc1ca6c62b9c0b4cbdcbf33af601042
jacklinlow <jacklinlow@hotmail.co.uk>
2016-02-20 18:18:26 +0000

    Create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, precence: true, length: {minimum: 3, maximum: 50 }
-  validates :description, precence: true, length: {minimum: 10, maximum: 300 }
+  #validates :title, precence: true, length: {minimum: 3, maximum: 50 }
+  #validates :description, precence: true, length: {minimum: 10, maximum: 300 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
-  get 'pages/home', to: 'pages#home'
-  get 'pages/about', to: 'pages#about'
+  #get 'pages/home', to: 'pages#home'
+  get 'about', to: 'pages#about'
 
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
